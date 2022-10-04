4c096d9c90909b7d8af978aa6ce201cc45846d06
Md Khan <md.monir.khan707@gmail.com>
2022-10-04 11:39:35 -0400

    added google and stripe key

--- config.php
+++ config.php
@@ -1,10 +1,18 @@
 <?php
 return [
 	'database' => [
-		'name' => 'task',
+		'name' => 'project-progress',
 		'username' => 'root',
 		'password'	=> '',
 		'connection' => 'mysql:host=127.0.0.1',
-		'options' => [ PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING]
+		'options' => [PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING]
+	],
+
+	'google_maps_api' => [
+		'key' => 'fdsfjdskjfksdjf2325425kjfdskfjdskfdsl',
+	],
+
+	'stripe_key' => [
+		'auth' => 'f40945ksdlfjdksfjdksfdsjsdkf'
 	]
 ];
